9c41fec3dd629f743a921d6bcf4523fc5a3ee8a5
GoranShekerov <g_sekerov@yahoo.com>
2014-09-25 14:01:04 +0200

    Create tpa-helper.h

---
+++ bake/source/ns-3.20/src/tpa/helper/tpa-helper.h
@@ -0,0 +1,14 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+#ifndef TPA_HELPER_H
+#define TPA_HELPER_H
+
+#include "ns3/tpa.h"
+
+namespace ns3 {
+
+/* ... */
+
+}
+
+#endif /* TPA_HELPER_H */
+
